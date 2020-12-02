8d16672d0124dddb34743fbabf24a87e08e6e82b
Brent Mitton <brent.mitton+github@gmail.com>
2020-12-02 14:48:12 -0500

    commit

---
+++ pages/dashboard.jsx
@@ -0,0 +1,15 @@
+export default function Dashboard() {
+
+    const foo = "here"
+
+  return (
+    <html>
+      <table>
+        <tr>
+          <td>Nice</td>
+          <td>{foo}</td>
+        </tr>
+      </table>
+    </html>
+  );
+}
